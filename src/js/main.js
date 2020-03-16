48c7b6ad2ce870e1ac543d71ab2b6767b77e2efc
Anna Mazurek <anna.mazurek.93@gmail.com>
2020-03-16 16:14:38 +0100

    Sort data by total income

--- src/js/main.js
+++ src/js/main.js
@@ -6,25 +6,31 @@ const table = document.querySelector('.table--js');
 fetch('https://recruitment.hal.skygate.io/companies')
   .then(res => res.json())
   .then(data => {
-    // console.log(data)
 
-    const tableRows = []
+    // const companies = [];
+    const tableRows = [];
+
     data.map(company => {
       fetch('https://recruitment.hal.skygate.io/incomes/' + company.id)
         .then(res => res.json())
         .then(res => {
           const companyIncome = res.incomes.map(income => parseFloat(income.value)).reduce((a, b) => a + b);
-console.log(company.id)
-          tableRows.push( `
-            <tr class="table__row">
-              <td class="table__data">${company.id}</td>
-              <td class="table__data">${company.name}</td>
-              <td class="table__data">${company.city}</td>
-              <td class="table__data">${companyIncome.toFixed(2)}</td>
-            </tr>
-          `);
+
+          tableRows.push({ 
+            income: companyIncome.toFixed(2),
+            content: `
+              <tr class="table__row">
+                <td class="table__data">${company.id}</td>
+                <td class="table__data">${company.name}</td>
+                <td class="table__data">${company.city}</td>
+                <td class="table__data">${companyIncome.toFixed(2)}</td>
+              </tr>
+            `
+          });
         })
         .then(res => {
+          tableRows.sort((a, b) => a.income - b.income)
+
           table.innerHTML = `
           <tr class="table__header">
             <th class="table__header table__header--id scope="col">Id</th>
@@ -32,7 +38,7 @@ console.log(company.id)
             <th class="table__header scope="col">City</th>
             <th class="table__header table__header--income scope="col">Total income</th>
           </tr>
-          ${tableRows}
+          ${tableRows.map(row => row.content).join('')}
     `;
         })
       })
