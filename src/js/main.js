65e1e2703a47edb3b37507596779d4556f387076
Anna Mazurek <anna.mazurek.93@gmail.com>
2020-03-17 18:26:19 +0100

    Feat: pagination

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,56 @@
 "use strict";
 
 const table = document.querySelector(".table__body--js");
+const form = document.querySelector(".form--js");
 const searchCompany = document.getElementById("filter");
+const select = document.querySelector(".form__select--js");
+const pagination = document.querySelector(".pagination--js");
 
-let companiesData = [];
-let filteredCompanies = [];
+let state = {
+  companiesData: [],
+  filteredCompanies: [],
+  page: 0,
+  perPage: 10,
+  loaded: false,
+}
+// let companiesData = [];
+// let filteredCompanies = [];
+// let page = 0;
+// let perPage = 10;
+// let loaded = false;
+if (!state.loaded) {
+  table.innerHTML = `<tr><td colspan="4" class="loader"><div class="loader__item"></div></td></tr>`;
+}
 
-searchCompany.onchange = handleChange;
+searchCompany.onkeypress = handleChange;
+// searchCompany.onchange = handleChange;
+form.onsubmit = handleSubmit;
+select.onchange = handleSelect;
 
 function handleChange(e) {
   setTimeout(() => {
     let userInput = e.target.value;
-    filteredCompanies = companiesData.filter(company =>
+    state.filteredCompanies = state.companiesData.filter(company =>
       company.name.toLowerCase().includes(userInput.toLowerCase())
     );
-    showData(filteredCompanies);
+    showData(state.filteredCompanies, state.perPage);
   }, 1000);
+}
+
+function handleSelect(e) {
+  state.perPage = parseInt(e.target.value);
+  showData(state.companiesData, state.perPage);
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
 }
 
 function fetchData() {
   fetch("https://recruitment.hal.skygate.io/companies")
     .then(res => res.json())
     .then(data => {
+      const dataLength = data.length;
       data.map(company => {
         fetch("https://recruitment.hal.skygate.io/incomes/" + company.id)
           .then(res => res.json())
@@ -30,48 +59,59 @@ function fetchData() {
               .map(income => parseFloat(income.value))
               .reduce((a, b) => a + b);
 
-            companiesData.push({
+              state.companiesData.push({
               id: company.id,
               name: company.name,
               city: company.city,
               income: companyIncome.toFixed(2)
             });
-            companiesData.sort((a, b) => b.income - a.income);
-            showData(companiesData);
-            // showData(company.id, company.name, company.city, companyIncome.toFixed(2))
-
-            // tableRows.push({
-            //   income: companyIncome.toFixed(2),
-            //   content: `
-            //     <tr class="table__row">
-            //       <td class="table__data">${company.id}</td>
-            //       <td class="table__data">${company.name}</td>
-            //       <td class="table__data">${company.city}</td>
-            //       <td class="table__data">${companyIncome.toFixed(2)}</td>
-            //     </tr>
-            //   `
-            // });
-            // const rows = tableRows.sort((a, b) => b.income - a.income).map(row => row.content).join('');
-            // table.innerHTML = `${rows}`;
+            if (state.companiesData.length === dataLength) {
+              state.companiesData.sort((a, b) => b.income - a.income);
+              showData(state.companiesData, 10);
+            }
           });
       });
     });
 }
 
-// function showData(id, name, city, income) {
-function showData(data) {
+function showData(data, perPage) {
+  const sliceFrom = 0 + perPage * state.page;
+  const sliceTo = perPage + perPage * state.page;
+  pagination.innerHTML = "";
+
   table.innerHTML = data
     .map(
       company => `
-      <tr class="table__row">
+        <tr class="table__row">
         <td class="table__data">${company.id}</td>
-        <td class="table__data">${company.name}</td>
-        <td class="table__data">${company.city}</td>
-        <td class="table__data">${company.income}</td>
-      </tr>
-  `
+          <td class="table__data">${company.name}</td>
+          <td class="table__data">${company.city}</td>
+          <td class="table__data">${company.income}</td>
+          </tr>
+          `
     )
+    .slice(sliceFrom, sliceTo)
     .join("");
+
+
+  /* HANDLE PAGINATION */
+  for (let i = 1; i <= state.companiesData.length / state.perPage; i++) {
+    pagination.innerHTML += `
+      <button class="pagination__button pagination__button--js">${i}</button>
+      `;
+  }
+
+  const buttons = Array.from(
+    document.querySelectorAll(".pagination__button--js")
+  );
+  
+  buttons.map(button => {
+    button.addEventListener("click", e => {
+      state.page = e.target.textContent - 1;
+      console.log(e.target.textContent);
+      showData(state.companiesData, state.perPage);
+    });
+  });
 }
 
 fetchData();
