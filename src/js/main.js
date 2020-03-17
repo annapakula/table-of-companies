65ecc49316a105efd8dbe6227e4f08c1edd46982
Anna Mazurek <anna.mazurek.93@gmail.com>
2020-03-17 13:35:12 +0100

    Feat: filter companies by name

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,77 @@
 "use strict";
 
-const table = document.querySelector('.table--js');
+const table = document.querySelector(".table__body--js");
+const searchCompany = document.getElementById("filter");
 
+let companiesData = [];
+let filteredCompanies = [];
 
-fetch('https://recruitment.hal.skygate.io/companies')
-  .then(res => res.json())
-  .then(data => {
+searchCompany.onchange = handleChange;
 
-    // const companies = [];
-    const tableRows = [];
+function handleChange(e) {
+  setTimeout(() => {
+    let userInput = e.target.value;
+    filteredCompanies = companiesData.filter(company =>
+      company.name.toLowerCase().includes(userInput.toLowerCase())
+    );
+    showData(filteredCompanies);
+  }, 1000);
+}
 
-    data.map(company => {
-      fetch('https://recruitment.hal.skygate.io/incomes/' + company.id)
-        .then(res => res.json())
-        .then(res => {
-          const companyIncome = res.incomes.map(income => parseFloat(income.value)).reduce((a, b) => a + b);
+function fetchData() {
+  fetch("https://recruitment.hal.skygate.io/companies")
+    .then(res => res.json())
+    .then(data => {
+      data.map(company => {
+        fetch("https://recruitment.hal.skygate.io/incomes/" + company.id)
+          .then(res => res.json())
+          .then(res => {
+            const companyIncome = res.incomes
+              .map(income => parseFloat(income.value))
+              .reduce((a, b) => a + b);
 
-          tableRows.push({ 
-            income: companyIncome.toFixed(2),
-            content: `
-              <tr class="table__row">
-                <td class="table__data">${company.id}</td>
-                <td class="table__data">${company.name}</td>
-                <td class="table__data">${company.city}</td>
-                <td class="table__data">${companyIncome.toFixed(2)}</td>
-              </tr>
-            `
+            companiesData.push({
+              id: company.id,
+              name: company.name,
+              city: company.city,
+              income: companyIncome.toFixed(2)
+            });
+            companiesData.sort((a, b) => b.income - a.income);
+            showData(companiesData);
+            // showData(company.id, company.name, company.city, companyIncome.toFixed(2))
+
+            // tableRows.push({
+            //   income: companyIncome.toFixed(2),
+            //   content: `
+            //     <tr class="table__row">
+            //       <td class="table__data">${company.id}</td>
+            //       <td class="table__data">${company.name}</td>
+            //       <td class="table__data">${company.city}</td>
+            //       <td class="table__data">${companyIncome.toFixed(2)}</td>
+            //     </tr>
+            //   `
+            // });
+            // const rows = tableRows.sort((a, b) => b.income - a.income).map(row => row.content).join('');
+            // table.innerHTML = `${rows}`;
           });
-        })
-        .then(res => {
-          tableRows.sort((a, b) => b.income - a.income)
+      });
+    });
+}
 
-          table.innerHTML = `
-          <tr class="table__header">
-            <th class="table__header table__header--id scope="col">Id</th>
-            <th class="table__header scope="col">Company</th>
-            <th class="table__header scope="col">City</th>
-            <th class="table__header table__header--income scope="col">Total income</th>
-          </tr>
-          ${tableRows.map(row => row.content).join('')}
-    `;
-        })
-      })
-  })
+// function showData(id, name, city, income) {
+function showData(data) {
+  table.innerHTML = data
+    .map(
+      company => `
+      <tr class="table__row">
+        <td class="table__data">${company.id}</td>
+        <td class="table__data">${company.name}</td>
+        <td class="table__data">${company.city}</td>
+        <td class="table__data">${company.income}</td>
+      </tr>
+  `
+    )
+    .join("");
+}
+
+fetchData();
